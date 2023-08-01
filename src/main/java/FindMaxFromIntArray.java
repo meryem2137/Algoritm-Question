ccdef28ecf910d0c23ee475f6472439c3265772d
meryem peker <meryem.peker2137@gmail.com>
2023-08-01 14:20:50 +0300

    Question1

--- src/main/java/FindMaxFromIntArray.java
+++ src/main/java/FindMaxFromIntArray.java
@@ -1,2 +1,30 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.OptionalInt;
+
 public class FindMaxFromIntArray {
+    public static void main(String[] args) {
+        int[] array = {6, 8, 3, 5, 1, 9};
+
+
+
+    }
+    public static int findMax(int[] arr){
+        return Arrays.stream(arr).max().orElse(Integer.MIN_VALUE);
+    }
+
+
+
+
+
 }
+
+
+
+
+/*
+Question-1 Find Max from int Array
+Write a function that can find the maximum number from an int Array.
+input: int[] array = {6, 8, 3, 5, 1, 9}; output: 9
+ */
