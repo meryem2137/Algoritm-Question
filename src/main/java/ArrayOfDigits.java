55646537a1d84b4fc8a0cb9fcfeb31ecdc4c1ba3
meryem peker <meryem.peker2137@gmail.com>
2023-08-01 21:19:56 +0300

    Question5

--- src/main/java/ArrayOfDigits.java
+++ src/main/java/ArrayOfDigits.java
@@ -1,2 +1,20 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class ArrayOfDigits {
+    public static void main(String[] args) {
+        int a=490;
+       int[] arr= arrayOfDigits(a);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    public static int[] arrayOfDigits(int arr){
+        return String.valueOf(arr)
+                .chars()
+                .map(Character::getNumericValue)
+                .toArray();
+
+    }
 }
